44aef25e8c9db547a1efb6e0fe9d6195216fb942
toto240325 <toto240325@gmail.com>
2021-07-04 21:41:33 +0200

    .

---
+++ webapp/src/components/EntityPlot/ChartSizeWrapper.tsx
@@ -0,0 +1,25 @@
+import React, { useRef, useEffect, useState } from "react";
+import Chart, { ChartProps } from "./Chart";
+
+type ChartSizeWrapperProps = Omit<Omit<ChartProps, "width">, "height"> & {
+  height: number;
+};
+
+const ChartSizeWrapper: React.FC<ChartSizeWrapperProps> = ({ height, ...props }) => {
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+  const [dimensions, setDimensions] = useState<[number, number]>([0, 0]);
+
+  useEffect(() => {
+    if (wrapperRef.current !== null) {
+      setDimensions([wrapperRef.current.clientWidth, wrapperRef.current.clientHeight]);
+    }
+  }, [wrapperRef]);
+
+  return (
+    <div ref={wrapperRef} style={{ height }}>
+      <Chart {...props} width={dimensions[0]} height={dimensions[1]} />
+    </div>
+  );
+};
+
+export default ChartSizeWrapper;
---
+++ whos_on_my_network/static/precache-manifest.9c04b15e620383815f205fa2196229ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed653fb957b62578b3820f9e03413d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66dc16b91fcb650fb018",
+    "url": "/static/css/2.3932ead9.chunk.css"
+  },
+  {
+    "revision": "9ad4bd6913a40d78fdec",
+    "url": "/static/css/main.2713c9c6.chunk.css"
+  },
+  {
+    "revision": "66dc16b91fcb650fb018",
+    "url": "/static/js/2.f6fcb243.chunk.js"
+  },
+  {
+    "revision": "78242a594233cede591e1f48e8e9cc18",
+    "url": "/static/js/2.f6fcb243.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ad4bd6913a40d78fdec",
+    "url": "/static/js/main.67c59005.chunk.js"
+  },
+  {
+    "revision": "187b41ded8aedf19c331",
+    "url": "/static/js/runtime-main.3e7c6de9.js"
+  },
+  {
+    "revision": "a91cedae6204cf1e0409dd77e8c7b762",
+    "url": "/static/media/logo.a91cedae.png"
+  }
+]);
